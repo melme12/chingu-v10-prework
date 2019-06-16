a2fefce7ec256c36d426418e5a1f506a4c59ae67
melme12 <32367959+melme12@users.noreply.github.com>
2019-06-16 16:23:06 +0200

    changed js function
    changed the way the function works, because I had a problem with double clicking before it worked

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-
-
-function hamBurger() {
-  const mainMenu = document.getElementById("main-menu");
-
-  if (mainMenu.style.display === "none") {
-    mainMenu.style.display = "block";
-  } else {
-    mainMenu.style.display = "none";
-  }
+function showHam () {  
+    var x = document.querySelector('#main-menu');
+    x.classList.toggle('show');
 }
 
 function showSub1() {
-  const subMenu1 = document.getElementById("sub-menu-1");
-
-  if (subMenu1.style.display === "none") {
-    subMenu1.style.display = "block";
-  } else {
-    subMenu1.style.display = "none";
-  }
+  var y = document.querySelector('#sub-menu-1');
+  y.classList.toggle('show1');
 }
+
+
+//Old Functions (problem with the double click in the beginning)
+// function hamBurger() {
+//   const mainMenu = document.getElementById("main-menu");
+
+//   if (mainMenu.style.display === "none") {
+//     mainMenu.style.display = "block";
+//   } else {
+//     mainMenu.style.display = "none";
+//   }
+// }
+
+// function showSub1() {
+//   const subMenu1 = document.getElementById("sub-menu-1");
+
+//   if (subMenu1.style.display === "none") {
+//     subMenu1.style.display = "block";
+//   } else {
+//     subMenu1.style.display = "none";
+//   }
+// }
