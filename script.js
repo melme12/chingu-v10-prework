03b6bff19d1b8d8851f36ee8b72ed4ea35977d0b
melme12 <32367959+melme12@users.noreply.github.com>
2019-06-16 20:52:51 +0200

    tried to change icons on click
    tried but didn't succeed in changing the icon on click

--- script.js
+++ script.js
@@ -5,27 +5,17 @@ function showHam () {
 
 function showSub1() {
   var y = document.querySelector('#sub-menu-1');
+  var toggleBtn = document.querySelector('#sub-menu-toggle-1');
+  
   y.classList.toggle('show1');
+  
+  var showState = "assets/icons/iconmonstr-plus-2-24.png";
+  
+  if ( showState == "assets/icons/iconmonstr-plus-2-24.png" ) {
+    document.images["showSub1img"].src = "assets/icons/iconmonstr-plus-2-24.png";
+    showState  = "earth.jpg";
+  } else {
+    document.images["showSub1img"].src = "assets/icons/iconmonstr-x-mark-2-24.png";
+    showState  = "assets/icons/iconmonstr-plus-2-24.png";
+  }
 }
-
-
-//Old Functions (problem with the double click in the beginning)
-// function hamBurger() {
-//   const mainMenu = document.getElementById("main-menu");
-
-//   if (mainMenu.style.display === "none") {
-//     mainMenu.style.display = "block";
-//   } else {
-//     mainMenu.style.display = "none";
-//   }
-// }
-
-// function showSub1() {
-//   const subMenu1 = document.getElementById("sub-menu-1");
-
-//   if (subMenu1.style.display === "none") {
-//     subMenu1.style.display = "block";
-//   } else {
-//     subMenu1.style.display = "none";
-//   }
-// }
